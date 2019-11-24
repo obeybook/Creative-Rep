87dae4ad47cd5ed7107b1afff627c9b808927ada
obeybook <obeybook@gmail.com>
2019-11-25 01:24:17 +0900

    ejs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+const fs = require('fs');
+const content1 = fs.readFileSync('src/html/main.ejs', 'utf8');
+
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 
 app.get('/', function (req, res) {
-  
+  fs.readFile(`src/html/main.ejs`,function(error,data){
+    if(error){
+      console.log(error);
+    }else{ 
+      var hello2 = ejs.render(content1,
+        {
+           message: "텍스트 메세지" 
+        }
+      ) 
+
+      res.writeHead(200,{'Content-Type': 'text/html'});
+      res.end(hello2);
+    }
+  })
 });
 
 app.listen(3000, function () {
