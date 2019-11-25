aae075f74863d1be47cb0dfe862ca17ad0f27bae
obeybook <obeybook@gmail.com>
2019-11-26 01:16:42 +0900

    퍼블리싱

--- App.js
+++ App.js
@@ -1,15 +1,13 @@
 const express = require('express');
-const app = express();
+const path = require('path');
 const app = express();
 const routes = require('./routes/index.js');
-app.use(express.static('public'));
 
-app.set('view engine', 'pug');
-app.set('views','./view'); // 뷰 폴더를 ./views 경로로 설정한다.
+app.set('view engine', 'pug'); // 템플릿 엔진 설정
+app.set('views', path.join(__dirname, './view'));  // 뷰 폴더를 ./views 경로로 설정한다.
 
-app.get('/', function (req, res) {
-  res.render('index');
-});
+app.use(express.static(path.join(__dirname, 'public'))); //동적 파일 경로 설정
+app.use('/', routes); //라우팅
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
