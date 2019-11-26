94f0354c01de7c81deb96af51a053cd49a0f0752
obeybook <obeybook@gmail.com>
2019-11-27 01:43:48 +0900

    퍼블리싱

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.render('index', {page:'Home', menuId:'home'});
+    res.render('index');
 });
     
 module.exports = router; 
