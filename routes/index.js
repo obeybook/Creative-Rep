25b3265c882374784c63d823fe5f58e5839150b9
obeybook <obeybook@gmail.com>
2019-12-01 17:27:21 +0900

    db

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,24 @@
 const express = require('express');
+var testDB = require("../lib/db.js")
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
     res.render('index');
 });
+
+router.get('/create', function(req, res, next) {
+    res.render('create');
+});
+
+router.post('/create_process', function(req, res, next) {
+    var post = req.body;
+    console.log(post)
+    testDB.query(`INSERT INTO maintest (text) VALUES(?)`, [post.text],
+     function(error, result, fields){
+         res.writeHead(302, {Location: `/`});
+         res.end();
+     })
+});
+
     
 module.exports = router; 
