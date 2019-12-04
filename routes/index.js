ec9136099a3493c584d3887c40ad7bf6f0be4733
obeybook <obeybook@gmail.com>
2019-12-04 17:59:32 +0900

    어려워

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
-var testDB = require("../lib/db.js")
+const app = express();
+const testDB = require("../lib/db.js")
+const bodyParser = require('body-parser');
 const router = express.Router();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 router.get('/', function(req, res, next) {
     res.render('index');
@@ -14,10 +18,10 @@ router.post('/create_process', function(req, res, next) {
     var post = req.body;
     console.log(post)
     testDB.query(`INSERT INTO maintest (text) VALUES(?)`, [post.text],
-     function(error, result, fields){
-         res.writeHead(302, {Location: `/`});
-         res.end();
-     })
+    function(error){
+        res.redirect([302], '/')
+        res.end();
+    })
 });
 
     
