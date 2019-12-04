9aa18bb9fb0df1a3d452d7373af690a6a2813f75
obeybook <obeybook@gmail.com>
2019-12-05 03:11:45 +0900

    db연동 테스트

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const bodyParser = require('body-parser');
 const routes = require('./routes/index.js');
 
 app.set('view engine', 'pug'); // 템플릿 엔진 설정
 app.set('views', path.join(__dirname, './view'));  // 뷰 폴더를 ./views 경로로 설정한다.
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, 'public'))); //동적 파일 경로 설정
 app.use('/', routes); //라우팅
--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,31 @@
 const express = require('express');
 const app = express();
-const testDB = require("../lib/db.js")
+const testDB = require("../lib/db.js");
 const bodyParser = require('body-parser');
 const router = express.Router();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 router.get('/', function(req, res, next) {
-    res.render('index');
+    testDB.query(`SELECT text FROM maintest`, function(error, list){
+        console.log(list[0].text)
+        res.render('index', {title: list});
+        next();
+    });
 });
 
 router.get('/create', function(req, res, next) {
     res.render('create');
+    next();
 });
 
 router.post('/create_process', function(req, res, next) {
     var post = req.body;
-    console.log(post)
-    testDB.query(`INSERT INTO maintest (text) VALUES(?)`, [post.text],
-    function(error){
-        res.redirect([302], '/')
+    testDB.query(`INSERT INTO maintest (text) VALUES(?)`, [post.text], function(error){
+        res.redirect('/');
         res.end();
-    })
+    });
+    next();
 });
 
     
