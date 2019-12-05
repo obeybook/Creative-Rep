0efd1ce0da49a23944aa4cb44c4b818fff466454
obeybook <obeybook@gmail.com>
2019-12-05 23:31:02 +0900

    에러처리

--- App.js
+++ App.js
@@ -12,6 +12,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public'))); //동적 파일 경로 설정
 app.use('/', routes); //라우팅
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.use(function(req, res, next) {
+  res.status(404).send('Sorry cant find that!');
+});
+ 
+app.use(function (err, req, res, next) {           
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+});
+ 
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!')
 });
