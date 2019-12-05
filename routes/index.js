c6fc403d0e7dedc2b3d5b3c963f3085b484e4fc0
obeybook <obeybook@gmail.com>
2019-12-06 02:17:41 +0900

    이미지 업로드 설정

--- routes/index.js
+++ routes/index.js
@@ -3,30 +3,37 @@ const app = express();
 const testDB = require("../lib/db.js");
 const bodyParser = require('body-parser');
 const router = express.Router();
+const multer = require('multer');
+const upload = multer({ dest: 'repository/img/' })
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 router.get('/', function(req, res, next) {
     testDB.query(`SELECT text FROM maintest`, function(error, list){
-        console.log(list[0].text)
         res.render('index', {title: list});
         next();
     });
 });
 
-router.get('/create', function(req, res, next) {
-    res.render('create');
+router.get('/works', function(req, res, next) {
+    res.render('worksCreate');
     next();
 });
 
-router.post('/create_process', function(req, res, next) {
-    var post = req.body;
-    testDB.query(`INSERT INTO maintest (text) VALUES(?)`, [post.text], function(error){
-        res.redirect('/');
-        res.end();
-    });
-    next();
-});
+router.post('/create_process', upload.single('userfile'), function(req, res){
+    res.send('Uploaded! : '+req.file); // object를 리턴함
+    console.log(req.file); // 콘솔(터미널)을 통해서 req.file Object 내용 확인 가능.
+  });
+
+// router.post('/create_process', function(req, res, next) {
+//     var post = req.body;
+//     testDB.query(`INSERT INTO maintest (text) VALUES(?)`, [post.text], function(error){
+        
+//     });
+//     res.redirect('/');
+//     next();
+// });
 
     
+
 module.exports = router; 
