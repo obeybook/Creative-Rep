3e8e52bf6259f23ecdda9577a772fc4326d6acd5
obeybook <obeybook@gmail.com>
2020-01-03 00:48:40 +0900

    login 구현 시작

--- App.js
+++ App.js
@@ -2,8 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
-const routes = require('./routes/index.js');
 const methodOverride = require('method-override');
+
+const routes = require('./routes/index.js');
+const auth = require('./routes/auth/auth.js');
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, './view'));  
@@ -14,7 +16,9 @@ app.use(bodyParser.json());
 app.use(methodOverride('_method'));
 
 app.use(express.static(path.join(__dirname, 'public'))); //동적 파일 경로 설정
+
 app.use('/', routes); //라우팅
+app.use('/auth', auth); //라우팅
 
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!')
