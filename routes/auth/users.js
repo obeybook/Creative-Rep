97d54a9e13633c1ca80af1348f32ab38ed5e8797
obeybook <obeybook@gmail.com>
2020-01-10 01:23:01 +0900

    회원가입 라우터 설정

--- routes/auth/users.js
+++ routes/auth/users.js
@@ -6,8 +6,13 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-router.get('/' ,function(req, res){
+router.get('/' , function(req, res){
     res.render('auth/login');
+    next();
+});
+
+router.get('/register', function(req, res){
+    res.render('auth/userManage/userRegister');
     next();
 });
 
