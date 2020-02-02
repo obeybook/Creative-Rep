b83296dff4717324f726949a32fb67f311ca92dd
obeybook <obeybook@gmail.com>
2020-02-02 23:52:22 +0900

    cookie study

--- App.js
+++ App.js
@@ -29,14 +29,18 @@ var http = require('http');
 var cookie = require('cookie');
 http.createServer(function(request, response){
   var cookies = {};
-  if( request.headers.coookie == undefined ){
+  if( request.headers.coookie !== undefined ){
     cookies = cookie.parse(request.headers.cookie);
     console.log(cookies);
   }else{
     console.log("fail");
   }
   response.writeHead(200, {
-      'Set-Cookie':['yummy_cookie=choco', 'tasty_cookie=strawberry']
-  });
+    'Set-Cookie':[
+        'yummy_cookie=choco', 
+        'tasty_cookie=strawberry',
+        `Permanent=cookies; Max-Age=${60*60*24*30}`
+    ]
+});
   response.end('Cookie!!');
 }).listen(3000);
