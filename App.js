2919d3f2ae15801d7e54b7537ba64ebe19a2e01a
obeybook <obeybook@gmail.com>
2020-02-03 23:53:28 +0900

    cookie study

--- App.js
+++ App.js
@@ -37,10 +37,12 @@ http.createServer(function(request, response){
   }
   response.writeHead(200, {
     'Set-Cookie':[
-        'yummy_cookie=choco', 
-        'tasty_cookie=strawberry',
-        `Permanent=cookies; Max-Age=${60*60*24*30}`
-    ]
+      'yummy_cookie=choco', 
+      'tasty_cookie=strawberry',
+      `Permanent=cookies; Max-Age=${60*60*24*30}`,
+      'Secure=Secure; Secure',
+      'HttpOnly=HttpOnly; HttpOnly'
+  ]
 });
   response.end('Cookie!!');
 }).listen(3000);
