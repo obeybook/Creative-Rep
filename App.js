af43aa7180bd7cad2b97a9f4d2fc9d855f81c93a
obeybook <obeybook@gmail.com>
2020-02-05 01:08:28 +0900

    세션 테스트

--- App.js
+++ App.js
@@ -1,47 +1,28 @@
-// const express = require('express');
-// const path = require('path');
-// const app = express();
-// const bodyParser = require('body-parser');
-// const cookieParser = require('cookie-parser');
-// const methodOverride = require('method-override');
+const express = require('express');
+const path = require('path');
+const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const methodOverride = require('method-override');
 
-// const routes = require('./routes/index.js');
-// const auth = require('./routes/auth/users.js');
+const routes = require('./routes/index.js');
+const auth = require('./routes/auth/users.js');
 
-// app.set('view engine', 'pug');
-// app.set('views', path.join(__dirname, './view'));  
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, './view'));  
 
-// app.use(cookieParser());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// app.use(methodOverride('_method')); 
+app.use(methodOverride('_method')); 
 
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', routes); 
-// app.use('/users', auth);  
-
-// app.listen(3000, function() {
-//   console.log('Example app listening on port 3000!')
-// });
-
-var express = require('express')
-var parseurl = require('parseurl')
-var session = require('express-session')
-
-var app = express()
-
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true
-}))
-
-app.get('/', function (req, res, next) {
-  res.send('hello session')
-})
+app.use('/', routes); 
+app.use('/users', auth);  
 
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!')
 });
+
--- routes/auth/users.js
+++ routes/auth/users.js
@@ -5,32 +5,42 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const jwt = require('jsonwebtoken');
-const token = jwt.sign({sub: 'asd1234'}, 'secret_key');
-console.log(token)
-
-const decoded_data = jwt.verify(token,'secret_key');
-console.log(decoded_data)
+const session = require('express-session')
+const FileStore = require('session-file-store')(session)
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  store: new FileStore() 
+}))
 
 router.get('/' , function(req, res){
-    const requeset_token = req.body.token;
-    try{
-        jwt.verify(token,'secret_key');
-        next();
-    }catch{
-        res.send('fail');
-    }
-    // let count = 121233;
-    // res.cookie('count', count);
-    // res.send(`Cookie : ${count}`);
-    // res.render('auth/login');
-    // next();
+    res.render('auth/login');
 });
 
 router.get('/register', function(req, res){
     res.render('auth/userManage/userRegister');
-    next();
 });
 
+let userData = {
+    id : 'test',
+    pw : '1111'
+}
+
+router.post('/login_test', function(req, res){
+    let post = req.body;
+    let _id = post.id;
+    let _pw = post.pw;
+
+    if( _id === userData.id && _pw === userData.pw){
+        // req.session.is_logined = true;
+        // req.session.id = userData.id;
+        // console.log(req.session.is_logined);
+        console.log(req.session);
+    }else{
+        console.log('실패');
+    }
+    
+})
 
 module.exports = router; 
