3d70f89d91a7ad8c5e465ddc1024d0c8c03089b2
obeybook <obeybook@gmail.com>
2020-04-10 01:26:30 +0900

    CORS 설정 추가

--- App.js
+++ App.js
@@ -4,30 +4,19 @@ const app = express();
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const methodOverride = require('method-override');
+const cors = require('cors');
 
-const session = require('express-session');
-const FileStore = require('session-file-store')(session)
-const compression = require('compression');
 const passport = require('./routes/auth/passport.js')(app);
 
 const routes = require('./routes/index.js');
 const auth = require('./routes/auth/auth.js');
 const users = require('./routes/auth/users.js');
 
+app.use(cors())
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(compression());
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  store: new FileStore(),
-  cookie:{
-    secure: false
-  }
-}))
 app.use(passport.initialize());
 app.use(passport.session());
 
